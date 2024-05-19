6845c1da5bc45a454b02a91b2993eafe112546fb
Kushtarbek <satarovkushtarbek05@gmail.com>
2024-05-19 15:18:30 +0600

    New Project

---
+++ myproject/store/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Goods
+
+class GoodsForm(forms.ModelForm):
+    class Meta:
+        model = Goods
+        fields = ['title', 'description', 'category', 'image', 'price', 'date']
